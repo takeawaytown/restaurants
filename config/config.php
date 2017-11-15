b7aba48d6f936fc179ab59f186a3bc5c7c9a89f8
Alex Scott <codetheorist@gmail.com>
2017-11-15 22:12:09 +0000

    Update config file

--- config/config.php
+++ config/config.php
@@ -2,13 +2,16 @@
 
 return [
     'invite_model' => TakeawayTown\Restaurants\Invite::class,
+    'restaurant_model' => TakeawayTown\Restaurants\Team::class,
     'team_model' => TakeawayTown\Restaurants\Team::class,
     'user_model' => config('auth.providers.users.model', App\User::class),
 
     'users_table' => 'users',
+    'restaurants_table' => 'restaurants',
     'teams_table' => 'teams',
-    'team_invites_table' => 'team_invites',
+    'restaurant_invites_table' => 'restaurant_invites',
 
+    'restaurant_user_table' => 'restaurant_user',
     'team_user_table' => 'team_user',
 
     'user_foreign_key' => 'id',
